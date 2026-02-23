4c5c38b87395fcf5431a748b8df8aeef84efd237
Julia Torseeva <torseeva.y@divan.ru>
2026-02-23 21:43:46 +0300

    add SUPABASE_CONFIG

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,7 @@
 // Fill this file with your Supabase project settings.
 // This file is safe to keep in public repo only with ANON key (never service role key).
 window.SUPABASE_CONFIG = {
-  url: "",
-  anonKey: "",
-  tableName: "people"
+  url: "https://wvywwisfgkkmzfaniadu.supabase.co",
+  anonKey: "sb_publishable_SZP5SP5hDsFZ-wjP5eFu_g_04DbkQzt",
+  tableName: "people",
 };
